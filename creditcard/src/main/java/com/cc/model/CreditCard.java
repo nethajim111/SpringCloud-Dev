ee56a182852f3a6f8771fbbf271b3098ce5006db
91800 <91800@DESKTOP-9T24OJK>
2020-02-07 13:12:57 +0530

    added code for actuator and swagger

--- creditcard/src/main/java/com/cc/model/CreditCard.java
+++ creditcard/src/main/java/com/cc/model/CreditCard.java
@@ -10,27 +10,34 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
 public class CreditCard {
 
 	@Id
 	@NotNull(message="Card Number Cannot be Null")
+	@ApiModelProperty(notes="credit card number",required = true)
 	private Long cardNumber;
 	
 	@NotNull(message="Card Holder Name Cannot be Null")
 	@NotEmpty(message="Card Holder Name Cannot be Empty")
 	@NotBlank(message="Card Holder Name Cannot be Blank")
+	@ApiModelProperty(notes="credit card holder name",required = true)
   	private String cardHolderName;
   	
 	@NotNull(message="CVV Cannot be Null")
 	@NotEmpty(message="CVV Cannot be Empty")
 	@NotBlank(message="CVV Cannot be blank")
+	@ApiModelProperty(notes="credit card 3 digit cvv number",required = true)
   	private String cvv;
   	
   	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
+  	@ApiModelProperty(notes="credit card expiry date")
   	private LocalDate expiryDate;
   	
   	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
+  	@ApiModelProperty(notes="credit card issue date")
   	private LocalDate issueDate;
   	
 	public Long getCardNumber() {
